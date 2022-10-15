1fe850402bc02a417b99c1ecd58e3499688da799
Akinfisoye <113646660+Akinfisoye@users.noreply.github.com>
2022-10-15 04:53:10 +0100

    alphac

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	char letter;
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-	putchar('\n');
-	return (0);
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
 }
