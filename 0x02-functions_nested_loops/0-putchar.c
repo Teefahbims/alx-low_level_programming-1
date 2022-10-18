50b570e0dd7b358b05cd6d333a05a83180c8249a
Erioluwa <eriakinfisoye@gmail.com>
2022-10-18 16:06:35 -0700

    tired

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
-#include"main.h"
-/**
-*main - Print _putchar
-*Return: Always(0); 
-*/
-=======
 #include "main.h"
-
 /**
- * main - Print _putchar
- * Return: Always 0
- */
->>>>>>> f610eb28e480f162b16b9d8477c282455250dfef
+*  * main - Print _putchar
+*   * Return: Always 0
+*/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
