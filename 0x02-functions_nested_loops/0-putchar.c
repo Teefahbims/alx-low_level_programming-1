9fab8912473609a8c764bbcd75d25bca4227addb
Erioluwa <eriakinfisoye@gmail.com>
2022-10-18 15:50:01 -0700

    omo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,8 @@
-#include "main.h"
+#include"main.h"
 /**
-*
-*  * main - Print _putchar
-*
-*   * Return: Always 0
-*
-*    */
+*main - Print _putchar
+*Return: Always(0); 
+*/
 int main(void)
 {
 _putchar('_');
