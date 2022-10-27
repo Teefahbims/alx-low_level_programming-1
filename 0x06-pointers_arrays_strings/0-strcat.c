eea51942eea71f078bccd72b0d366eb46aab970a
Akinfisoye <113646660+Akinfisoye@users.noreply.github.com>
2022-10-28 00:08:36 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,10 @@
 #include "main.h"
 /**
- *
- *  * _strcat - concat 2 string
- *
- *   * @dest:char
- *
- *    * @src:char
- *
- *     * Return:char
-*/
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
+ */
 char *_strcat(char *dest, char *src)
 {
 char *s = dest;
